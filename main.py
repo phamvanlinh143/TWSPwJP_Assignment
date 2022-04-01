ef8d246320bfeb1296ad019ed794926df7cbe1f7
PhamVanLinh <phamvanlinh143@gmail.com>
2022-04-01 20:18:10 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from twsp_object import Window, Machine, Job, INF_TIME, TWSPwJP
 
+
 def run():
+    # -------------- initial data --------------
     split_min = 3
     J1 = Job(process_time=6, split_min=split_min)
     J2 = Job(process_time=7, split_min=split_min)
@@ -12,13 +14,11 @@ def run():
     LPT_lst_job = sorted(lst_jobs, key=lambda x: x.process_time, reverse=True)
     SPT_lst_job = sorted(lst_jobs, key=lambda x: x.process_time, reverse=False)
 
-
     W11 = Window(index=1, start_time=0, end_time=7)
     W12 = Window(index=2, start_time=7, end_time=12)
     W13 = Window(index=3, start_time=12, end_time=20)
     W14 = Window(index=4, start_time=20, end_time=INF_TIME)
     lst_window_m1 = [W11, W12, W13, W14]
-
     M1 = Machine(num_wins=len(lst_window_m1), windows=lst_window_m1, split_min=split_min)
 
     W21 = Window(index=1, start_time=0, end_time=5)
@@ -26,8 +26,8 @@ def run():
     W23 = Window(index=3, start_time=11, end_time=18)
     W24 = Window(index=4, start_time=18, end_time=INF_TIME)
     lst_window_m2 = [W21, W22, W23, W24]
-
     M2 = Machine(num_wins=len(lst_window_m2), windows=lst_window_m2, split_min=split_min)
+    # -------------- end initial data --------------
 
     solver = TWSPwJP(jobs=lst_jobs, machines=[M1, M2], split_min=split_min)
 
@@ -35,7 +35,7 @@ def run():
     cmax = 0
     for machine in solver.machines:
         for window in machine.windows:
-            if window.pivot_time > cmax:
+            if len(window.assigned_jobs) > 0 and window.pivot_time > cmax:
                 cmax = window.pivot_time
     print(f"Cmax = {cmax}")
     solver.reset()
@@ -45,7 +45,7 @@ def run():
     cmax = 0
     for machine in solver.machines:
         for window in machine.windows:
-            if window.pivot_time > cmax:
+            if len(window.assigned_jobs) > 0 and window.pivot_time > cmax:
                 cmax = window.pivot_time
     print(f"LPT Cmax = {cmax}")
     solver.reset()
@@ -55,7 +55,7 @@ def run():
     cmax = 0
     for machine in solver.machines:
         for window in machine.windows:
-            if window.pivot_time > cmax:
+            if len(window.assigned_jobs) > 0 and window.pivot_time > cmax:
                 cmax = window.pivot_time
     print(f"SPT Cmax = {cmax}")
     solver.reset()
