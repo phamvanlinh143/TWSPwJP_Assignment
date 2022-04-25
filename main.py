0f530ada80868b04ed0e048c570e757b35d06997
PhamVanLinh <phamvanlinh143@gmail.com>
2022-04-25 17:16:52 +0700

    Update visualization

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from twsp_object import TWSPwJP
-from twsp_object import Job, Window, Machine, INF_TIME
 import os
 import sys
 import time
-from utils import load_json_data, visualize
+import argparse
 
-out = sys.stdout
+from typing import List
+from pathlib import Path
+from utils import load_json_data, visualize
+from twsp_object import TWSPwJP
+from twsp_object import Job, Window, Machine, INF_TIME
+
 
 def parse_dataset(json_path):
     data = load_json_data(json_path)
@@ -34,43 +37,44 @@ def parse_dataset(json_path):
 
 
 def run(file: str):
-    with open(file.split('/')[-2] + ".txt", 'a') as f:
-        sys.stdout = f
-        split_min, lst_jobs, lst_machines = parse_dataset(file)
-        for job in lst_jobs:
-            print(job)
-        for machine in lst_machines:
-            print(machine)
+    split_min, lst_jobs, lst_machines = parse_dataset(file)
+    for job in lst_jobs:
+        print(job)
+    for machine in lst_machines:
+        print(machine)
 
-        solver = TWSPwJP(jobs=lst_jobs, machines=lst_machines, split_min=split_min)
-        print(f"Lower Bound = {solver.LB}")
+    input_name = Path(file).stem
+    print("=" * 80)
+    solver = TWSPwJP(jobs=lst_jobs, machines=lst_machines, split_min=split_min)
+    print(f"Lower Bound = {solver.LB}")
 
-        start_time = time.time()
-        solver.solve()
-        print(f"Elapsed time: {time.time() - start_time}")
-        print(f"FCFS Cmax = {solver.cmax}")
-        solver.reset()
-        print("=" * 80)
-        solver.tabu_search(limit_time=30, tabu_size=4096)
-        print(f"Tabu Search Cmax = {solver.cmax}")
-        solver.reset()
-        print("=" * 80)
-        solver.hill_climbing(limit_time=60)
-        print(f"Hill Climbing Cmax = {solver.cmax}")
-        solver.reset()
-        print("=" * 80)
-        solver.local_beam_search(limit_time=60, beam_width=5)
-        print(f"Local Beam Search Cmax = {solver.cmax}")
-        print("=" * 80)
-        for machine in solver.machines:
-            print(machine.machine_name)
-            for window in machine.windows:
-                print(window)
-        visualize(solver.machines, lst_jobs, image_name='input_10_2_3_5')
-        solver.reset()
-        sys.stdout = out
+    print("=" * 80)
+    start_time = time.time()
+    solver.solve()
+    print(f"elapsed time: {time.time() - start_time}")
+    print(solver.solution_info())
+    print(f"FCFS Cmax = {solver.cmax}")
+    visualize(solver.machines, lst_jobs, image_name=f'{input_name}', prefix="FCFS")
+    solver.reset()
+    print("=" * 80)
+    solver.tabu_search(limit_time=30, tabu_size=4096)
+    print(f"Tabu Search Cmax = {solver.cmax}")
+    visualize(solver.machines, lst_jobs, image_name=f'{input_name}', prefix="Tabu")
+    print("=" * 80)
+    solver.reset()
+    solver.hill_climbing(limit_time=30)
+    print(f"Hill Climbing Cmax = {solver.cmax}")
+    visualize(solver.machines, lst_jobs, image_name=f'{input_name}', prefix="Hill")
+    print("=" * 80)
+    solver.reset()
+    solver.local_beam_search(limit_time=30, beam_width=5)
+    visualize(solver.machines, lst_jobs, image_name=f'{input_name}', prefix="Beam")
+    print(f"Local Beam Search Cmax = {solver.cmax}")
+    print("=" * 80)
+    solver.reset()
 
-def get_files(dirname: str) -> list[str]:
+
+def get_files(dirname: str) -> List[str]:
     res = []
     for name in os.listdir(dirname):
         f = os.path.join(dirname, name)
@@ -80,24 +84,31 @@ def get_files(dirname: str) -> list[str]:
             res += get_files(f)
     return res
 
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, help='data_path', default='dataset/DS1/10_2_3/input_10_2_3_5.json')
+    args = parser.parse_args()
     # run('dataset/DS0/input1.json')
     # run('dataset/DS0/input2.json')
     # run('dataset/DS0/input3.json')
-    dirs = ['dataset/DS1/10_2_3',
-    'dataset/DS1/10_3_3',
-    'dataset/DS1/10_4_3',
-    'dataset/DS1/20_2_3',
-    'dataset/DS1/20_3_3',
-    'dataset/DS1/20_4_3',
-    'dataset/DS1/30_2_3',
-    'dataset/DS1/30_3_3',
-    'dataset/DS1/30_4_3',]
-    for dir in dirs:
-        for name in os.listdir(dir):
-            f = os.path.join(dir, name)
-            if os.path.isfile(f):
-                run(f)
+    # dirs = ['dataset/DS1/10_2_3',
+    #         'dataset/DS1/10_3_3',
+    #         'dataset/DS1/10_4_3',
+    #         'dataset/DS1/20_2_3',
+    #         'dataset/DS1/20_3_3',
+    #         'dataset/DS1/20_4_3',
+    #         'dataset/DS1/30_2_3',
+    #         'dataset/DS1/30_3_3',
+    #         'dataset/DS1/30_4_3', ]
+    # for dir in dirs:
+    #     for name in os.listdir(dir):
+    #         f = os.path.join(dir, name)
+    #         if os.path.isfile(f):
+    #             run(f)
+
+    run(args.data_path)
+
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import numpy as np
 from mycolorpy import colorlist as mcp
+from pathlib import Path
 
 
 def load_json_data(json_path):
@@ -46,16 +47,17 @@ def get_neighbor_index(lst_job_size):
 
     return list(neighbor_index)
 
-def visualize(machines, lst_jobs, image_name='image'):
-    color1=mcp.gen_color(cmap="tab20",n=len(lst_jobs))
+
+def visualize(machines, lst_jobs, image_name='image', prefix="fcfs"):
+    color1 = mcp.gen_color(cmap="tab20", n=len(lst_jobs))
     job_color_dict = {}
-    for idx,job in enumerate(lst_jobs):
+    for idx, job in enumerate(lst_jobs):
         job_color_dict[job.job_name] = color1[idx]
-    
+
     machine_idx_map = {}
     for idx, machine in enumerate(machines):
         machine_idx_map[machine.machine_name] = idx
-    
+
     plot_arr = []
     for machine in machines:
         for window in machine.windows:
@@ -64,39 +66,46 @@ def visualize(machines, lst_jobs, image_name='image'):
                 res = {}
                 res['Machine'] = machine.machine_name
                 res['Duration'] = sub_job.p_time
-                res['Job']= sub_job.parent_name
+                res['Job'] = sub_job.parent_name
                 res['Start'] = start_time
                 res['Finish'] = start_time + sub_job.p_time
                 res['Color'] = job_color_dict[sub_job.parent_name]
                 start_time = res['Finish']
                 plot_arr.append(res)
-    
+
     df = pd.DataFrame(plot_arr)
     max_finish = max(df['Finish'])
-    fig, ax = plt.subplots(len(machines),1,figsize=(12, 5+(len(lst_jobs)+len(machines))/4))
-    for idx,machine in enumerate(machines):
-        ax[idx].set_ylim([0,0.5])
-        ax[idx].set_xlim([0,max_finish + 2])
+    fig, ax = plt.subplots(len(machines), 1, figsize=(12, 5 + (len(lst_jobs) + len(machines)) / 4))
+    for idx, machine in enumerate(machines):
+        ax[idx].set_ylim([0, 0.5])
+        ax[idx].set_xlim([0, max_finish + 2])
         ax[idx].xaxis.set_ticks(np.arange(0, max_finish + 2, 2))
         ax[idx].set_yticklabels([])
-        ax[idx].set_ylabel(machine.machine_name,rotation=0)
-        ax[idx].set_xlabel('Time',rotation=0)
+        ax[idx].set_ylabel(machine.machine_name, rotation=0)
+        ax[idx].set_xlabel('Time', rotation=0)
         ax[idx].spines['top'].set_visible(False)
         ax[idx].spines['right'].set_visible(False)
         ax[idx].plot(1, 0, ">k", transform=ax[idx].get_yaxis_transform(), clip_on=False)
-    #     ax[idx].plot(0, 1, "^k", transform=ax[idx].get_xaxis_transform(), clip_on=False)
+        #     ax[idx].plot(0, 1, "^k", transform=ax[idx].get_xaxis_transform(), clip_on=False)
         for window in machine.windows:
-            ax[idx].axvline(window.end_time,ymax=0.7,color='red',linewidth=3.2)
+            ax[idx].axvline(window.end_time, ymax=0.7, color='red', linewidth=3.2)
 
     for item in plot_arr:
-        rectangle = Rectangle((item['Start'],0),item['Finish'] - item['Start'],0.25,color=item['Color'],alpha=0.25,linewidth=2.5)
+        rectangle = Rectangle((item['Start'], 0), item['Finish'] - item['Start'], 0.25, color=item['Color'], alpha=0.25,
+                              linewidth=2.5)
         ax[machine_idx_map[item['Machine']]].add_patch(rectangle)
         rx, ry = rectangle.get_xy()
-    #     cx = rx + rectangle.get_width()/2.0
-    #     cy = ry + rectangle.get_height()/2.0
-        cx = rx + rectangle.get_width()/2.0 + 0.24
+        #     cx = rx + rectangle.get_width()/2.0
+        #     cy = ry + rectangle.get_height()/2.0
+        cx = rx + rectangle.get_width() / 2.0 + 0.24
         cy = ry + rectangle.get_height() + 0.03
-        ax[machine_idx_map[item['Machine']]].annotate(item['Job'] + ' = ' + str(item['Duration']), (cx, cy), color='black', weight='bold', fontsize=9, ha='center', va='center')
+        ax[machine_idx_map[item['Machine']]].annotate(item['Job'] + ' = ' + str(item['Duration']), (cx, cy),
+                                                      color='black', weight='bold', fontsize=9, ha='center',
+                                                      va='center')
     fig.set_facecolor('white')
-    plt.savefig(image_name + '.png')
-    
+    output_dir = Path(f'outputs/{image_name}')
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True, exist_ok=True)
+    output_name = output_dir.joinpath(f"{prefix}.png")
+    plt.savefig(str(output_name))
+
